4c91eb762cb6e3e5eef1bbc9bb954965df09d764
Henry Bersey <henry@bersey.com>
2022-02-26 15:08:12 +0000

    Added tailwind, removed placeholder stuff

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#9506CB"
+      }
+    },
+  },
+  plugins: [],
+}
