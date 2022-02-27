6268694110d695de2bcdfbb2b68558f5c4b39c60
Henry Bersey <henry@bersey.com>
2022-02-27 12:28:03 +0000

    Responsive Home Page

--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,14 @@ export default () => {
           <img src="/Logo LG NO BG.png" className="object-contain h-full cursor-pointer" />
         </Link>
 
-        <div className="flex-grow" />
+        {/* <div className="flex-grow" />
 
         <Link href="https://tamis.app/">
           <a className="text-white">Sign In</a>
-        </Link>
+        </Link> */}
       </header>
 
-      <main className="overflow-y-scroll snap-y snap-mandatory flex-grow scroll-smooth">
+      <main className="overflow-y-scroll snap-y snap-proximity lg:snap-mandatory flex-grow scroll-smooth">
 
         <section className="h-full relative snap-center" id="home">
           <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center space-y-4">
@@ -46,16 +46,16 @@ export default () => {
 
         </section>
 
-        <section className="h-full bg-primary text-white flex snap-center" id="about">
-          <div className="w-1/2 h-2/3 m-auto grid grid-cols-3 gap-x-8">
+        <section className="lg:h-full bg-primary text-white flex snap-start lg:snap-center pt-4 pb-8 lg:pt-0 lg:pb-0" id="about">
+          <div className="w-4/5 lg:w-1/2 xl:h-2/3 m-auto lg:grid grid-cols-3 gap-x-8">
 
-            <h1 className="text-4xl font-semibold text-right">About Us</h1>
+            <h1 className="text-4xl font-semibold lg:text-right">About Us</h1>
             <p className="col-span-2 text-justify">
               TAMIS is a modern management information system hoping to revolutionise the way that schools operate.
               Built with a finely tuned combination of both reliable, well-proven software architecture as well as new, innovative solutions, TAMIS aims to change the game with its shared goals in both usability and efficiency.
             </p>
 
-            <h2 className="text-3xl text-right">Who Are We?</h2>
+            <h2 className="text-3xl lg:text-right mt-4 lg:mt-0">Who Are We?</h2>
             <div className="col-span-2 flex flex-col">
               <p className="text-justify">
                 I started developing TAMIS in late 2021.
@@ -66,7 +66,7 @@ export default () => {
               <em className="font-light text-right">Henry Bersey, Creator of TAMIS</em>
             </div>
 
-            <h2 className="text-3xl text-right">What's Next?</h2>
+            <h2 className="text-3xl lg:text-right mt-4 lg:mt-0">What's Next?</h2>
             <div className="col-span-2 flex flex-col">
               <p className="text-justify">
                 Currently, TAMIS is in the process of implementing the final stages of development before begging the first stage of user testing, Pre-Alpha v0.1.0.
@@ -74,15 +74,15 @@ export default () => {
                 Most importantly the TAMIS team are excited about the future of what started as a project but is now forming into a reality.
               </p>
               <Link href="#pre-alpha">
-                <a className="text-right pt-2 font-medium">Read More About Pre-Alpha.</a>
+                <a className="text-right lg:pt-2 font-medium">Read More About Pre-Alpha.</a>
               </Link>
             </div>
           </div>
         </section>
 
         <section className="h-full flex snap-center text-gray-500" id="pre-alpha">
-          <div className="m-auto w-2/5 text-center flex flex-col space-y-2">
-            <h2 className="text-3xl text-black">User Testing Is Starting Soon!</h2>
+          <div className="m-auto w-4/5 lg:w-2/5 text-center flex flex-col space-y-2">
+            <h2 className="text-3xl text-black mb-2">User Testing Is Starting Soon!</h2>
             <p className="mx-auto">
               A closed Pre-Alpha will start within the next two months.
               In this first iteration of testing we'll be focusing on the pupil database, user system and the initial design and feel of the TAMIS Staff Portal.
